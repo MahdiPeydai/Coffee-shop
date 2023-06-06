8322466f3af947e1dbaff7bb724dd784f59983c7
MahdiPeydai <mahdipeydai@gmail.com>
2023-06-07 00:14:05 +0330

    create product feature added

--- app/helpers/forms.py
+++ app/helpers/forms.py
@@ -1,5 +1,6 @@
-from wtforms import Form, StringField, PasswordField, DateField, FileField, TimeField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import Form, StringField, PasswordField, DateTimeLocalField, FileField, TextAreaField, SelectField, \
+    SelectMultipleField, DateField, IntegerField, widgets, TimeField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange
 from flask_wtf.file import FileSize, FileAllowed, FileRequired
 from app import app, curs
 
@@ -63,3 +64,27 @@ class UpdateCategory(Form):
                                            FileAllowed(app.config['IMAGE_EXTENSION'])])
     category_active = SelectField('وضعیت',
                                   choices=['فعال', 'غیرفعال'])
+
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+
+class CreateProduct(Form):
+    product_name = StringField('نام',
+                               validators=[DataRequired(),
+                                           Length(max=50)])
+    product_category = MultiCheckboxField('دسته بندی',
+                                          choices=[])
+    product_quantity = IntegerField('تعداد',
+                                    validators=[DataRequired()])
+    product_price = StringField('قیمت',
+                                validators=[DataRequired(),
+                                            Length(max=50)])
+    product_discount = IntegerField('نخفیف',
+                                    validators=[NumberRange(min=0, max=100)], )
+    product_discount_date = DateTimeLocalField('تاریخ تخفیف',
+                                               format='%m/%d/%y')
+    product_short_description = TextAreaField('توضیحات',
+                                              validators=[Length(max=255)])
--- app/routs/admin.py
+++ app/routs/admin.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint, url_for, flash, redirect, request, make_response
 from app import app, db, curs
-from app.helpers.forms import CreateCategory, UpdateCategory
+from app.helpers.forms import CreateCategory, UpdateCategory, CreateProduct
 import uuid
 import os
 
@@ -15,11 +15,60 @@ def administrator():
     menu_type = request.args.get('type')
     form = None
     if menu_type == 'product':
-        product_sql = 'SELECT product.id, product.name, product.quantity, product.price, product.discount, product.discount_date, category.id as category_id' \
+        form = CreateProduct(request.form)
+        if request.method == 'POST' and request.args.get('form_type') == 'create':
+            product_category_str = request.form.getlist('product_category')
+            product_category = []
+            for i in product_category_str:
+                product_category.append(int(i))
+
+            create_product_sql = 'INSERT INTO product (name, price, quantity, discount, discount_date, short_description) ' \
+                                 'VALUES (%s, %s, %s, %s, %s, %s) '
+            create_product_val = (request.form['product_name'], request.form['product_price'],
+                                  request.form['product_quantity'], request.form['product_discount'],
+                                  request.form['product_discount_date'], request.form['product_short_description'])
+            curs.execute(create_product_sql, create_product_val)
+            db.commit()
+            product_id_sql = 'SELECT id ' \
+                             'FROM product ' \
+                             'WHERE name = %s'
+            product_id_val = (request.form['product_name'],)
+            curs.execute(product_id_sql, product_id_val)
+            product_id = curs.fetchone()['id']
+            for category_id in product_category:
+                product_category_sql = 'INSERT INTO product_category(product_id, category_id) ' \
+                                       'VALUES (%s, %s)'
+                product_category_val = (product_id, category_id)
+                curs.execute(product_category_sql, product_category_val)
+                db.commit()
+            flash('محصول با موفقیت اضافه شد', 'message')
+
+        product_sql = 'SELECT product.id, product.name, product.quantity, product.price, product.discount, product.discount_date, category.name as category_name ' \
                       'FROM product ' \
                       'INNER JOIN product_category on product.id = product_category.product_id ' \
                       'INNER JOIN category on product_category.category_id = category.id'
-        data = curs.execute(product_sql)
+        curs.execute(product_sql)
+        products = curs.fetchall()
+        data = {}
+        for product in products:
+            if product['id'] not in data.keys():
+                data[product['id']] = {
+                    'name': product['name'],
+                    'quantity': product['quantity'],
+                    'price': product['price'],
+                    'discount': product['discount'],
+                    'discount_date': product['discount_date'],
+                    'category': [product['category_name']]
+                }
+            else:
+                data[product['id']]['category'].append(product['category_name'])
+        category_name_sql = 'SELECT id, name ' \
+                            'FROM category'
+        curs.execute(category_name_sql)
+        categories = []
+        for category in curs.fetchall():
+            categories.append((category['id'], category['name']))
+        form.product_category.choices = categories
     elif menu_type == 'category':
         form = CreateCategory(request.form)
 
@@ -39,29 +88,21 @@ def administrator():
             category_image_name = f'{str(uuid.uuid4())}{file_format}'
             category_image.save(os.path.join(app.config['CATEGORY_IMAGE_FOLDER'], category_image_name))
 
-            if request.form['category_parent'] == '':
-                category_parent = 0
-            else:
-                parent_id_sql = 'SELECT id ' \
-                                'FROM category ' \
-                                'WHERE name=%s'
-                parent_id_val = (request.form['category_parent'],)
-                curs.execute(parent_id_sql, parent_id_val)
-                category_parent = curs.fetchone()['id']
-
             create_category_sql = 'INSERT INTO category (name, description, image, active, parent_id) ' \
                                   'VALUES (%s, %s, %s, 1, %s)'
-            create_category_val = (request.form['category_name'], request.form['category_description'], category_image_name, category_parent)
+            create_category_val = (
+                request.form['category_name'], request.form['category_description'], category_image_name,
+                request.form['category_parent'])
             curs.execute(create_category_sql, create_category_val)
             db.commit()
             flash('دسته بندی با موفقیت ایجاد شد', 'message')
 
-        category_name_sql = 'SELECT name ' \
+        category_name_sql = 'SELECT id, name ' \
                             'FROM category'
         curs.execute(category_name_sql)
-        categories = ['']
-        for i in curs.fetchall():
-            categories.append(i['name'])
+        categories = [(0, '')]
+        for category in curs.fetchall():
+            categories.append((category['id'], category['name']))
         form.category_parent.choices = categories
 
         category_sql = 'SELECT A.id, A.name, A.description, A.image, B.name AS parent_name, A.active ' \
@@ -93,28 +134,18 @@ def administrator():
 @admin.route('/admin/category/<int:category_id>/update', methods=['GET', 'POST'])
 def category_update(category_id):
     if request.method == 'POST':
-        if request.form['category_parent'] != '':
-            parent_id_sql = 'SELECT id ' \
-                            'FROM category ' \
-                            'WHERE name=%s'
-            parent_id_val = (request.form['category_parent'],)
-            curs.execute(parent_id_sql, parent_id_val)
-            parent_id = curs.fetchone()['id']
-        else:
-            parent_id = 0
-
 
         if request.form['category_active'] == 'فعال':
             category_active = 1
         else:
             category_active = 0
 
-
         category_update_sql = 'UPDATE category ' \
                               'SET name=%s, parent_id=%s, description=%s, active=%s ' \
                               'WHERE id=%s'
         category_update_val = (
-            request.form['category_name'], parent_id, request.form['category_description'], category_active,
+            request.form['category_name'], request.form['category_parent'], request.form['category_description'],
+            category_active,
             category_id)
         curs.execute(category_update_sql, category_update_val)
         db.commit()
@@ -142,12 +173,12 @@ def category_update(category_id):
         flash('نغییرات با موفقیت انجام شد', 'message')
         return redirect(url_for('admin.administrator', type='category', category_id=category_id))
 
-    category_name_sql = 'SELECT name ' \
+    category_name_sql = 'SELECT id, name ' \
                         'FROM category'
     curs.execute(category_name_sql)
-    categories = [None]
-    for i in curs.fetchall():
-        categories.append(i['name'])
+    categories = [(0, '')]
+    for category in curs.fetchall():
+        categories.append((category['id'], category['name']))
 
     category_sql = 'SELECT A.id, A.name, A.description, A.image, B.name AS parent_name, A.active ' \
                    'FROM category A ' \
