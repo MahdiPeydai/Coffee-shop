f9a1440f05a71937c33705a155b110cd90ad0c59
MahdiPeydai <mahdipeydai@gmail.com>
2023-07-02 18:43:48 +0330

    description added to product and removed from permission

--- src/js/category.js
+++
@@ -1,7 +0,0 @@
-import '../css/category.css'
-
-const priceElements = document.getElementsByClassName('price');
-for (let i = 0; i < priceElements.length; i++) {
-    const priceValue = priceElements[i].innerHTML;
-    priceElements[i].innerHTML = priceValue.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
--- src/js/user_update.js
+++
@@ -1,7 +0,0 @@
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../css/user_update.css'
